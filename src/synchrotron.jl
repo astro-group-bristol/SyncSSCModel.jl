2ec9ca49b67db01600dbf4338d55308b48d9fd1a
Gloria Raharimbolamena <100141757+GloriaRAH@users.noreply.github.com>
2022-05-20 15:42:25 +0100

    Fixing some bugs (#13)
    * Added the synchrotron power spectral flux calculation and changing parameters values using Schwartz et al 2000 and Tavecchio et al 2000 papers.
    PKS0637-752
    
    * Update on the last commit
    
    * Add some changes on the documentation
    
    * Updated beta and dL calculations
    
    Fixed a bug with B versus beta. Updated dL to use cgs units for Hubble constant. Put blob radius into the list of parameters.
    
    * Making update via Phajy Last update on adding radius parameters and changing units
    
    * Update
    
    * Test #1
    
    * Fixing some bugs on my repository
    
    * Fixing some bug with my repository #2
    
    Co-authored-by: Andy Young <andy.young@bristol.ac.uk>

--- src/synchrotron.jl
+++ src/synchrotron.jl
@@ -57,7 +57,7 @@ function S_syn(ϵ, mps)
     # doppler factor D(Γ,θ)
     # Γ is the Bulk Lorentz Factor
     # Θ is the angle between the direction of the blob's motion and the direction to the observer
-    # B = 1 / β in the Dermer e tal. (1997) paper
+    # B = 1 / β in the Dermer et al. (1997) paper
     β = sqrt(1.0 - 1.0/mps.Γ^2)
     μ_obs = cos(mps.θ)
     D = 1.0 / (mps.Γ*(1 - μ_obs/β))
