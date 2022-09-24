567d761c7341a7574e600fe112dc0e5d9258ea96
Gloria Raharimbolamena <gloria.raharimbolamena@bristol.ac.uk>
2022-09-24 21:00:48 +0100

    Fixing bug #mps

--- test/plotTest.jl
+++ test/plotTest.jl
@@ -2,7 +2,7 @@ using DiscJetConnections
 using Parameters
 
 # Set up parameters, changing mass to 10^7 M_Sun
-mps = MyParamStruct(M8=1.0E8)
+mps = MyParamStruct(M_BH=1.0E8)
 
 # Test Synchrotron plot
 syncPlot(mps)
