26815228694f08947d0393d3032acd6a417fa1fe
phajy <andy.young@bristol.ac.uk>
2023-04-24 11:10:46 +0100

    Updated fitting examples (#33)
    * Updated free parameters for Pic A fit
    
    * Update to PKS fitting
    
    * Updated PKS example
    
    * Changed dL and radius to logarithmic parameters

--- examples/PicA.jl
+++ examples/PicA.jl
@@ -3,27 +3,29 @@
 using SyncSSCModel
 using SpectralFitting
 using Plots
+using Revise
 
 # define the model we want to fit
 # includes default parameter values
 # specifies which paramters are free
 function PicASSCModel(;
-    K = FitParam(1.0),
-    B = FitParam(3.3E-5),
-    n_e0 = FitParam(5.3),
-    radius = FitParam(7.7E20),
+    K = FitParam(1.0, lower_limit = 0.5, upper_limit = 2.0),
+    B = FitParam(3.3E-5, lower_limit = 0.0, upper_limit = 1.0E-2),
+    n_e0 = FitParam(5.3, lower_limit = 0.0, upper_limit = 1.0E2),
+    log_radius = FitParam(log10(7.7E20), lower_limit = 20.0, upper_limit = 22.0),
     Γ = FitParam(1.0),
     γ_min = FitParam(8.7E1),
     γ_max = FitParam(1.0E6),
     p = FitParam(2.48),
-    dL = FitParam(4.752E26),
+    log_dL = FitParam(log10(4.752E26), lower_limit = 26.0, upper_limit = 28.0),
     θ = FitParam(23.0 * pi / 180.0),
     z = FitParam(0.035),
 )
     SSCModel{
         typeof(K),
         # SpectralFitting.FreeParameters{(:K, :B, :p, :radius, :θ, :dL, :z, :n_e0)},
-        SpectralFitting.FreeParameters{(:K, :B, :p, :θ, :n_e0,)},
+        # SpectralFitting.FreeParameters{(:K, :B, :p, :θ, :n_e0,)},
+        SpectralFitting.FreeParameters{(:B, :p, :n_e0, :log_radius, :log_dL)},
     }(
         K,
         B,
@@ -32,9 +34,9 @@ function PicASSCModel(;
         γ_min,
         γ_max,
         Γ,
-        radius,
+        log_radius,
         θ,
-        dL,
+        log_dL,
         z,
     )
 end
@@ -96,11 +98,12 @@ prob = FittingProblem(model, dataset)
 res = fit(prob, LevenbergMarquadt(), autodiff = :finite)
 # using OptimizationOptimJL
 # res = fit(prob, ChiSquared(), NelderMead())
-# print the result prettily
-display(res)
 
 plot(dataset.x, dataset.y, seriestype = :scatter, xscale = :log10, yscale = :log10, mc=:red, xrange=(1e7, 1e26), yrange=(1e-14, 1e-11), legend = :topleft, label = "Data")
 # plot!(res, lc=:blue)
 
 f = invokemodel(νrange, model, res.u)
 plot!(νrange, f, lc=:blue, label = "Best fit model")
+
+# print the result prettily
+display(res)
